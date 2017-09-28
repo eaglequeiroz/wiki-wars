1b52f054e7810fc6cdb5ea3c35fa9ba69b4aeed3
Igor Queiroz <igorcenourox@gmail.com>
2017-09-28 00:00:45 -0300

    Just a mockup of the People Page

--- src/pages/People.js
+++ src/pages/People.js
@@ -1,10 +1,93 @@
 import React from 'react';
+import github from '../assets/images/home/social-github-pic.png'
 
 export default class People extends React.Component {
   render() {
+
     return (
       <div>
-        <h1>Welcome to the Characters Page!</h1>
+
+        <div>
+          <div className="ui input">
+            <input type="text" placeholder="Search for your character here..." className="inputSearch" />
+          </div>
+            <button className="ui button">Search</button>
+        </div>
+
+        <div className="container">
+
+        <div className="ui five column grid container">
+
+          <div className="column">
+            <figure>
+              <img className="socialIcons ui image" src={github} alt=""/>
+              <figcaption>Character Name</figcaption>
+            </figure>
+          </div>
+          <div className="column">
+            <figure>
+              <img className="socialIcons ui image" src={github} alt=""/>
+              <figcaption>Character Name</figcaption>
+            </figure>
+          </div>
+          <div className="column">
+            <figure>
+              <img className="socialIcons ui image" src={github} alt=""/>
+              <figcaption>Character Name</figcaption>
+            </figure>
+          </div>
+          <div className="column">
+            <figure>
+              <img className="socialIcons ui image" src={github} alt=""/>
+              <figcaption>Character Name</figcaption>
+            </figure>
+          </div>
+          <div className="column">
+            <figure>
+              <img className="socialIcons ui image" src={github} alt=""/>
+              <figcaption>Character Name</figcaption>
+            </figure>
+          </div>
+
+          <div className="column">
+            <figure>
+              <img className="socialIcons ui image" src={github} alt=""/>
+              <figcaption>Character Name</figcaption>
+            </figure>
+          </div>
+          <div className="column">
+            <figure>
+              <img className="socialIcons ui image" src={github} alt=""/>
+              <figcaption>Character Name</figcaption>
+            </figure>
+          </div>
+          <div className="column">
+            <figure>
+              <img className="socialIcons ui image" src={github} alt=""/>
+              <figcaption>Character Name</figcaption>
+            </figure>
+          </div>
+          <div className="column">
+            <figure>
+              <img className="socialIcons ui image" src={github} alt=""/>
+              <figcaption>Character Name</figcaption>
+            </figure>
+          </div>
+          <div className="column">
+            <figure>
+              <img className="socialIcons ui image" src={github} alt=""/>
+              <figcaption>Character Name</figcaption>
+            </figure>
+          </div>
+
+        </div>
+        <div className="centerBox"  >
+            <i className="arrow left huge icon" aria-hidden="true"></i>
+            <i className="arrow right huge icon" aria-hidden="true"></i>
+        </div>
+
+      </div>
+
       </div>
     );
   }
